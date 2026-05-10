4e242bad331cbf7796d0ebba814cc0f987161d12
nehuen-hervias <nehuenhervias@gmail.com>
2026-05-10 00:57:09 -0300

    Agrego test del div

--- tests/test_div.py
+++ tests/test_div.py
@@ -10,6 +10,18 @@ def test_div_normal():
     """Ejemplo: 6 / 3 debe dar 2.0."""
     assert div(6, 3) == 2.0
 
+def test_div_por_cero():
+    with pytest.raises(ZeroDivisionError):
+        div(10, 0)
+
+@pytest.mark.parametrize("a,b,expected", [
+    (7, 2, 3.5),
+    (-6, 3, -2.0),
+])
+def test_div_parametrizado(a, b, expected):
+    assert div(a, b) == expected
+
+
 
 # --- TU TURNO ---
 # Agregá tests para los siguientes casos:
